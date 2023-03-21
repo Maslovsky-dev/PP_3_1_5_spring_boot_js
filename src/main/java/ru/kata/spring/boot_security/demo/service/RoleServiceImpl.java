a98389c0e33ac2c635ace08fb2bb8fa86ef3a31e
Maslovsky-dev <maslovskyvn.nano@ya.ru>
2023-03-21 23:56:11 +0300

    Add RoleService and Impl

--- src/main/java/ru/kata/spring/boot_security/demo/service/RoleServiceImpl.java
+++ src/main/java/ru/kata/spring/boot_security/demo/service/RoleServiceImpl.java
@@ -1,4 +1,39 @@
 package ru.kata.spring.boot_security.demo.service;
 
-public class RoleServiceImpl {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ru.kata.spring.boot_security.demo.model.Role;
+import ru.kata.spring.boot_security.demo.repository.RoleRepository;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
+@Service
+public class RoleServiceImpl implements RoleService {
+    private final RoleRepository roleRepository;
+    @Autowired
+    public RoleServiceImpl(RoleRepository roleRepository) {
+        this.roleRepository = roleRepository;
+    }
+
+    @Transactional
+    @Override
+    public List<Role> findAll() {
+        return roleRepository.findAll();
+    }
+    @Transactional
+    @Override
+    public Optional<Role> findById(String roleName) {
+        return roleRepository.findById(roleName);
+    }
+    @Transactional
+    @Override
+    public void save(Role role) {
+        roleRepository.save(role);
+    }
+    @Transactional
+    @Override
+    public void deleteById(String roleName) {
+        roleRepository.deleteById(roleName);
+    }
 }
